4cdeeb8be70ccebd85f8b8ff9761b2e08626f564
Mercy <mercy@Mercys-MacBook-Air.local>
2026-05-24 18:38:30 +0200

    Add dated multi-city weather files

--- ingestion/Weather_api_ingestion.py
+++ ingestion/Weather_api_ingestion.py
@@ -1,22 +1,28 @@
 import requests
-import pandas as pd
-
-# Stockholm coordinates
-latitude = 59.3293
-longitude = 18.0686
-
-# API URL
-url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,wind_speed_10m_max&timezone=auto"
-
-# Request data from API
-response = requests.get(url)
-
-# Convert response to JSON
-data = response.json()
-
 import json
+from datetime import datetime
 
-with open("raw_data/stockholm_weather.json", "w") as file:
-    json.dump(data, file, indent=4)
+cities = {
+    "stockholm": {"latitude": 59.3293, "longitude": 18.0686},
+    "gavle": {"latitude": 60.6749, "longitude": 17.1413},
+    "lulea": {"latitude": 65.5848, "longitude": 22.1547}
+}
 
-print("Weather data saved successfully!")
+today_date = datetime.today().strftime("%Y-%m-%d")
+
+for city, coordinates in cities.items():
+    latitude = coordinates["latitude"]
+    longitude = coordinates["longitude"]
+
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,wind_speed_10m_max&timezone=auto"
+
+    response = requests.get(url)
+    data = response.json()
+
+    file_path = f"raw_data/{city}_weather_{today_date}.json"
+
+    with open(file_path, "w") as file:
+        json.dump(data, file, indent=4)
+
+    print(f"{city} weather data saved successfully!")
+    
