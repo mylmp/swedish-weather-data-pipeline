9cb047c126c5eb976575548cfb9daf76a4367861
mylmp <miheret.lampa@hyperisland.se>
2026-06-03 22:35:26 +0200

    Update Weather_api_ingestion.py

--- ingestion/Weather_api_ingestion.py
+++ ingestion/Weather_api_ingestion.py
@@ -1,6 +1,16 @@
 import requests
 import json
 from datetime import datetime
+import os
+from dotenv import load_dotenv
+from azure.storage.blob import BlobServiceClient
+
+load_dotenv()
+
+connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+container_name = "weather-raw"
+
+blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
 cities = {
     "stockholm": {"latitude": 59.3293, "longitude": 18.0686},
@@ -22,7 +32,16 @@ for city, coordinates in cities.items():
     file_path = f"raw_data/{city}_weather_{today_date}.json"
 
     with open(file_path, "w") as file:
-        json.dump(data, file, indent=4)
+        json.dump(data, file, indent=4) 
+    blob_name = f"{city}_weather_{today_date}.json"
 
-    print(f"{city} weather data saved successfully!")
-    
+    blob_client = blob_service_client.get_blob_client(
+        container=container_name,
+        blob=blob_name
+    )
+
+    with open(file_path, "rb") as data_file:
+        blob_client.upload_blob(data_file, overwrite=True)
+
+    print(f"{city} weather data uploaded to Azure Blob Storage!")
+
